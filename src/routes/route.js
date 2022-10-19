0f6a25838713a5c36be3f5047be057a5b55298fc
sachin3348 <sachin.mahto@functionup.org>
2022-10-19 05:42:32 +0530

    solution of assignment 4

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,56 +2,17 @@ const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    res.send('My second ever api!')
-});
-
-router.get('/students', function (req, res){
-    
-    console.log("The path params in the request are : ", req.query) // query
-    let students = ['Sabiha', 'Neha', 'Akash']
-    const index = req.query.index
-    res.send(students[index])
-})
 
 
-router.get('/student-list/:x', function (req, res){
-    
-    let students = ['Sabiha', 'Neha', 'Akash']
-    const index = req.params.index
-    res.send(students[index])
-})
 
-
-// Example 1 for path params
-router.get('/students/:studentName', function(req, res){
-    // ':' denotes that the following part of route is a variable
-    // The value of this variable is what we are sending in the request url after /students
-    // This value is set in the form of an object inside req.params
-    // The object contain key value pairs
-    // key is the variable in the route
-    // value is whatever dynamic value sent in the request url
-    let myParams = req.params
-
-    // params attribute is fixed in a request object
-    // params contains the path parameters object
-    console.log("The path params in the request are : ", myParams)
-    res.send('The full name is ' + myParams.studentName )
-})
-
-// Example 2 for path params
-router.get('/student-details/:x', function(req, res){
+router.get('/student/index', function(req, res){
     let requestParams = req.params
     console.log("This is the request ", requestParams)
     let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
     res.send('Dummy response')
 })
 
 module.exports = router;
-
 
 /**
  * get
